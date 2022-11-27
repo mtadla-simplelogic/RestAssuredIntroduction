bbf6c06995623eae3e15031c4f310778ff949280
Mateusz Tadla <mat@scalepoint.com>
2022-11-27 14:48:37 +0100

    ex2

--- src/test/java/GetAllUsers.java
+++ src/test/java/GetAllUsers.java
@@ -6,4 +6,13 @@ zrobic get go /users
 i sprawdzić, ze odpowiedz ma status code 200
 
      */
+
+        /*
+
+zrobic get do /user/1
+
+sprawdzamy JSON Pathem czy  "lat": "-37.3159",
+
+uruchumiamy logowane dla response i request
+     */
 }
