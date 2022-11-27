148381ac52db8e64bc11a5816d391c1c413a1bd2
Mateusz Tadla <mat@scalepoint.com>
2022-11-27 14:47:02 +0100

    zad1

--- src/test/java/GetAllPosts.java
+++ src/test/java/GetAllPosts.java
@@ -1,5 +1,9 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class GetAllPosts {
@@ -8,7 +12,38 @@ public class GetAllPosts {
     public void shouldGetAllPosts() {
         when()
                 .get("https://jsonplaceholder.typicode.com/posts").
-        then()
-                .statusCode(200);
+                then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+    @Test
+    public void shouldGetFirstPost() {
+        when()
+                .get("https://jsonplaceholder.typicode.com/posts/1").
+                then()
+                .statusCode(200)
+                .log()
+                .all();
+    }
+
+    @Test
+    public void shouldGetFirstPostAndValidateJson() {
+        Response response =
+                given().
+                        log().
+                        all().
+                when()
+                        .get("https://jsonplaceholder.typicode.com/posts/1").
+                then()
+                        .statusCode(200)
+                        .log()
+                        .all()
+                        .extract().response();
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Assert.assertEquals(jsonPath.get("title"), "sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
     }
 }
